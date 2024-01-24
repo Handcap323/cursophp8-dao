4a6493d09cf4bc6e1efa3ae3225f4468e387ad25
Handcap323 <diiego2512@gmail.com>
2024-01-24 10:43:44 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php 
 
 require_once("config.php");
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById("3");
+//echo $root;
 
-$root = new Usuario();
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$root->loadById("3");
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("oo");
+//echo json_encode($search);
 
-echo $root;
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("boot", "abcdefg");
+echo $usuario;
  ?>
